18292f7fb501fbd790dd329ef94ad343879f2c01
JustSnow <justsnow17@yandex.ru>
2019-09-14 13:07:48 +0300

    change folders structure

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
-import indexRouter from './routes/index'
-import usersRouter from './routes/users'
+import indexRouter from './app/routes/index'
+import usersRouter from './app/routes/users'
 
 const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'app/views'))
 app.set('view engine', 'pug')
 
 app.use(logger('dev'))
@@ -23,12 +23,12 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) =>
   next(createError(404))
-})
+)
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
--- config/config.js
+++ config/config.js
@@ -1,28 +1,28 @@
 require('dotenv').config()
 
 module.exports = {
-  "development": {
-    "username": "postgres",
-    "password": null,
-    "database": "english_blog_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "operatorsAliases": false
+  'development': {
+    'username': 'postgres',
+    'password': null,
+    'database': 'english_blog_development',
+    'host': '127.0.0.1',
+    'dialect': 'postgres',
+    'operatorsAliases': false
   },
-  "test": {
-    "username": "postgres",
-    "password": null,
-    "database": "english_blog_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "operatorsAliases": false
+  'test': {
+    'username': 'postgres',
+    'password': null,
+    'database': 'english_blog_test',
+    'host': '127.0.0.1',
+    'dialect': 'postgres',
+    'operatorsAliases': false
   },
-  "production": {
+  'production': {
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
-    "dialect": "postgres",
-    "operatorsAliases": false
+    'dialect': 'postgres',
+    'operatorsAliases': false
   }
 }
