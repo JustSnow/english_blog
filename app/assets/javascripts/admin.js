70f55ae791213402bb494aac1550597db696d6e9
JustSnow <justsnow17@yandex.ru>
2019-10-13 14:04:38 +0300

    continue with admin page layout

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,2 +1,11 @@
 require('../stylesheets/admin.sass')
-console.log('admin.js');
+require('jquery/dist/jquery.slim.min')
+
+require('bootstrap/js/src/util')
+require('bootstrap/js/src/tools/sanitizer')
+
+require('bootstrap/js/src/tooltip')
+require('bootstrap/js/src/collapse')
+require('bootstrap/js/src/tab')
+require('bootstrap/js/src/modal')
+require('bootstrap/js/src/dropdown')
