b420a752e47d0be1be1cce1a271ae1e33f988cd8
JustSnow <justsnow17@yandex.ru>
2019-10-30 19:50:13 +0300

    show content category with contents

--- app/controllers/layout/content_categories.js
+++ app/controllers/layout/content_categories.js
@@ -2,8 +2,20 @@ import db from '../../models'
 import createError from 'http-errors'
 
 class ContentCategoriesController {
-  static async show(req, res) {
+  static async show(req, res, next) {
+    const { id } = req.params
 
+    try {
+      db.contentCategory.findByPk(id).then(contentCategory => {
+        if (contentCategory === null) { throw new createError.NotFound() }
+
+        contentCategory.getContents().then(contents => {
+          res.render('content_categories/show', { contentCategory, contents })
+        }).catch(next)
+      }).catch(next)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
